e2090e8d2525d699d5f148e7416b484842ce70ef
Paweł Kujda <pawel.kujda@onet.pl>
2018-07-02 20:25:31 +0200

    Second Stage Pizza Class Abstract PizzaBuilder

---
+++ PizzaBuilder.java
@@ -0,0 +1,8 @@
+abstract public class PizzaBuilder {
+
+    protected Pizza pizza;
+
+    abstract public void BuildDough();
+    abstract public void BuildSauce();
+    abstract public void BuildTopping();
+}
